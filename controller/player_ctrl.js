0424973900ab0a624c90f5e693c7f75c90acb3fd
vvamshi6 <vvamshi6@gmail.com>
2016-09-01 18:53:44 +0530

    JavaScript

--- controller/player_ctrl.js
+++ controller/player_ctrl.js
@@ -1,23 +1,11 @@
-angular.module('iplApp').controller('player_ctrl',demo);
-function demo($scope,$firebaseObject){
-  var ref = firebase.database().ref();
-// console.log(data11)
-var syncObject = $firebaseObject(ref);
-  // synchronize the object with a three-way data binding
-  // click on `index.html` above to see it used in the DOM!
-  syncObject.$loaded().then(function(result){
-     //console.log(result.DelhiDareDevils);
-     //console.log(syncObject);
-     $scope.data = result.DelhiDareDevils;
-    var array = [];
-    $scope.setData = function(key){
-     str = key.replace(/\s/g,'');
-     $scope.data = result[str];
-     console.log($scope.data);
-    }
-      //$scope.data = data;
-    $scope.images = {};
-    $scope.teams = result.tean_info;
-  });
-// putting a console.log here won't work, see below
+var iplApp = angular.module('iplApp');
+iplApp.controller('player_ctrl', retrieveData);
+
+function retrieveData($scope, $firebaseObject, $rootScope, TeamService) {
+    var ref = firebase.database().ref(TeamService.getData());
+    var syncObject = $firebaseObject(ref);
+    syncObject.$loaded().then(function(result) {
+        $scope.data = result;
+        $scope.images = {};
+    });
 };
--- controller/team_ctrl.js
+++ controller/team_ctrl.js
@@ -1,1 +1,28 @@
-angular.module('iplApp').controller('team_ctrl',demo);
+var iplApp = angular.module('iplApp');
+iplApp.controller('team_ctrl', retrieveData);
+
+function retrieveData($scope, $firebaseObject, $rootScope, TeamService) {
+    var ref = firebase.database().ref('tean_info');
+    var syncObject = $firebaseObject(ref);
+    syncObject.$loaded().then(function(result) {
+        console.log(result);
+        $scope.data = result;
+        $scope.teamName = function(name) {
+        console.log(name);
+        TeamService.setData(name);
+        }
+    });
+    var storage = firebase.storage();
+    var storageRef = firebase.storage().ref();
+    console.log(storageRef);
+    storageRef.child('DelhiDareDevils/amit_mishra.png').getDownloadURL().then(function(url) {
+      console.log(url);
+      // Get the download URL for 'images/stars.jpg'
+  // This can be inserted into an <img> tag
+  // This can also be downloaded directly
+  }).catch(function(error) {
+      console.log("Error"+error);
+    });
+
+
+};
